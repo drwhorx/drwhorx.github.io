ce437bfbdaadefd369d1db6956f0c94f484cf607
Amon Green <drwhorx@gmail.com>
2019-02-11 17:09:21 -0500

    kajhdkasjdhasldk

--- strat2019/js/main.js
+++ strat2019/js/main.js
@@ -1,22 +1,138 @@
 // use json file as website structure, then use a variable as current subnav
-window.onload = function () {
-    $.getJSON("https://drwhorx.github.io/strat2019/js/organize.json", function (data) {
-        sessionStorage.setItem("data", JSON.stringify(data))
-        const arr = Object.keys(data)
-        for (i = 0; i < arr.length; i++) {
-            var keys = Object.keys(data[arr[i]])
-            for (b = 0; b < keys.length; b++) {
-                var element = document.createElement("input")
-                element.setAttribute("type", "text")
-                element.setAttribute("cat", arr[i])
-                element.width = "25px"
-                element.height = "10px"
-                element.style = "font-size: 8px; left: " + data[arr[i]][keys[b]].x + "px; top: " + data[arr[i]][keys[b]].y + "px; position: absolute"
-                element.placeholder = keys[b]
-                document.getElementById("items").appendChild(element)
-            }
+const data = {
+    "Sandstorm": {
+        "Level 2 Start (1)": {
+            "x": 290,
+            "y": 250,
+            "opts": []
+        },
+        "Level 2 Start (2)": {
+            "x": 290,
+            "y": 377,
+            "opts": []
+        },
+        "Level 1 Start (1)": {
+            "x": 350,
+            "y": 272,
+            "opts": []
+        },
+        "Level 1 Start (2)": {
+            "x": 350,
+            "y": 313,
+            "opts": []
+        },
+        "Level 1 Start (3)": {
+            "x": 350,
+            "y": 355,
+            "opts": []
+        },
+        "Rocket 1": {
+            "x": 510,
+            "y": 130,
+            "opts": []
+        },
+        "Rocket 2": {
+            "x": 510,
+            "y": 499,
+            "opts": []
+        },
+        "Cargo Ship": {
+            "x": 565,
+            "y": 313,
+            "opts": []
         }
-    })
+    },
+    "Teleop": {
+        "Rocket 1": {
+            "x": 510,
+            "y": 130,
+            "opts": []
+        },
+        "Rocket 2": {
+            "x": 510,
+            "y": 499,
+            "opts": []
+        },
+        "Cargo Ship": {
+            "x": 565,
+            "y": 313,
+            "opts": []
+        },
+        "Defense": {
+            "x": 850,
+            "y": 313,
+            "opts": []
+        }
+    },
+    "Endgame": {
+        "Rocket 1": {
+            "x": 510,
+            "y": 130,
+            "opts": []
+        },
+        "Rocket 2": {
+            "x": 510,
+            "y": 499,
+            "opts": []
+        },
+        "Cargo Ship": {
+            "x": 565,
+            "y": 313,
+            "opts": []
+        },
+        "Defense": {
+            "x": 850,
+            "y": 313,
+            "opts": []
+        },
+        "Level 3 Climb": {
+            "x": 290,
+            "y": 303,
+            "opts": []
+        },
+        "Level 2 Climb (1)": {
+            "x": 290,
+            "y": 250,
+            "opts": []
+        },
+        "Level 2 Climb (2)": {
+            "x": 290,
+            "y": 377,
+            "opts": []
+        },
+        "Level 1 End (1)": {
+            "x": 350,
+            "y": 272,
+            "opts": []
+        },
+        "Level 1 End (2)": {
+            "x": 350,
+            "y": 323,
+            "opts": []
+        },
+        "Level 1 End (3)": {
+            "x": 350,
+            "y": 355,
+            "opts": []
+        },
+    }
+}
+window.onload = function () {
+    sessionStorage.setItem("data", JSON.stringify(data))
+    const arr = Object.keys(data)
+    for (i = 0; i < arr.length; i++) {
+        var keys = Object.keys(data[arr[i]])
+        for (b = 0; b < keys.length; b++) {
+            var element = document.createElement("input")
+            element.setAttribute("type", "text")
+            element.setAttribute("cat", arr[i])
+            element.width = "25px"
+            element.height = "10px"
+            element.style = "left: " + data[arr[i]][keys[b]].x + "px; top: " + data[arr[i]][keys[b]].y + "px;"
+            element.placeholder = keys[b]
+            document.getElementById("items").appendChild(element)
+        }
+    }
     setInterval(function checkPeriod() {
         var arr = Object.keys(JSON.parse(sessionStorage.getItem("data")))
         var i = arr.indexOf($("#gameStatus").val())
@@ -26,7 +142,14 @@ window.onload = function () {
         document.getElementById(arr[1] + "Canvas").hidden = true
         var status = $("#gameStatus").val()
         var children = document.getElementById("items").children
+        var align = ($("#alliance").val() == "Red" ? "left: " : "right: ")
+        var opp = (align == "left: " ? "right: " : "left: ")
         for (i = 0; i < children.length; i++) {
+            children[i].removeAttribute("width")
+            children[i].removeAttribute("height")
+            if (children[i].getAttribute("style").indexOf(align) == -1) {
+                children[i].setAttribute("style", children[i].getAttribute("style").replace(opp, align))
+            }
             if (children[i].getAttribute("cat") != status) {
                 children[i].hidden = true
             } else {
