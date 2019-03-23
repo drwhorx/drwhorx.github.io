341e40f40c181a8b3077b5d21e70d60b44a72c72
Amon Green <drwhorx@gmail.com>
2019-03-23 09:29:06 -0400

    alskdnalsdkn

--- refresh/main.js
+++ refresh/main.js
@@ -22,13 +22,13 @@ function compare() {
         alert("Refreshed!")
     );
 }
-function matches() {
+function compMatches() {
     var jqxhr = $.ajax({
         url: "https://script.google.com/macros/s/AKfycbz-R1qyevrkZwcHv-KuQIzcjsPOnch_YbI6N3on0y0D55raAvIV/exec",
         method: "GET",
         dataType: "json",
         data: {
-            data: "Compare Matches"
+            data: "Matches"
         }
     }).success(
         alert("Refreshed!")
