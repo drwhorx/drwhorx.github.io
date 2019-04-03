e7e0feb3f9a4e88dbd278735870bd1d07a564126
Amon Green <drwhorx@gmail.com>
2019-04-03 19:33:04 -0400

    ljalskjaskdj

--- refresh/main.js
+++ refresh/main.js
@@ -1,36 +1,18 @@
-function rankings() {
-    var jqxhr = $.ajax({
-        url: "https://script.google.com/macros/s/AKfycbz-R1qyevrkZwcHv-KuQIzcjsPOnch_YbI6N3on0y0D55raAvIV/exec",
-        method: "GET",
-        dataType: "json",
-        data: {
-            data: "Rankings"
-        }
-    }).success(
-        alert("Refreshed!")
-    );
+window.onload = function() {
+    var arr = ["getTeamHistory","getSum","getEventRankingsByTeam","getTitles","updateTBA","TBAConfig","getSettings","getArr","getLast","combineData","compare","parseAct","compareMatches","strategy","main","suffix","percentRank","eventMatches","predict","addCol","onChange","getItem","getEntries","setAll","getMatches","getEventTeamsKeys","eventPredict","eventOPRS","getTeam","getEvent","getTeamEventListKeys","getEventRankings","getYearsParticipated","getTBA"]
+    for (i = 0; i < arr.length; i++) {
+        document.getElementById("select").innerHTML += ("<option>" + arr[i] + "</option>")
+    }
 }
-function compare() {
+function submit() {
     var jqxhr = $.ajax({
         url: "https://script.google.com/macros/s/AKfycbz-R1qyevrkZwcHv-KuQIzcjsPOnch_YbI6N3on0y0D55raAvIV/exec",
         method: "GET",
         dataType: "json",
         data: {
-            data: "Compare"
+            data: document.getElementById("select").value
         }
     }).success(
-        alert("Refreshed!")
-    );
-}
-function compMatches() {
-    var jqxhr = $.ajax({
-        url: "https://script.google.com/macros/s/AKfycbz-R1qyevrkZwcHv-KuQIzcjsPOnch_YbI6N3on0y0D55raAvIV/exec",
-        method: "GET",
-        dataType: "json",
-        data: {
-            data: "Matches"
-        }
-    }).success(
-        alert("Refreshed!")
+        alert("Function sent!")
     );
 }
